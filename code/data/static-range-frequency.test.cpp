0a34d21bb34af5d2452808cb495e19ededddca5e
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-05-12 21:13:54 +0200

    Add more wavelet tree tests

---
+++ code/data/static-range-frequency.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_frequency"
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#include "WaveletTree.h"
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  auto b = a;
+  sort(b.begin(), b.end());
+  node t(a.begin(), a.end(), b.begin());
+  while (q--) {
+    int l, r, k;
+    cin >> l >> r >> k;
+    cout << t.freq(l, r, k) << '\n';
+  }
+}
+ 
