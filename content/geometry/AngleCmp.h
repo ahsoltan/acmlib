4d1a4d7f84749f1d5e61db2a4b179092f1e990c7
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-05-28 17:28:39 +0200

    New Point and AngleCmp

---
+++ content/geometry/AngleCmp.h
@@ -0,0 +1,15 @@
+/**
+ * Opis: Sortuje punkty w kolejności CCW, zaczynając od $y < 0$.
+ * Punkt $(0, 0)$ należy do linii $x \geq 0$, $y = 0$.
+ */
+#pragma once
+
+int half(P a) {
+  if (sgn(a.y) < 0) return -1;
+  if (sgn(a.y) == 0 && sgn(a.x) >= 0) return 0;
+  return 1;
+}
+bool angle_cmp(P a, P b) {
+  if (half(a) != half(b)) return half(a) < half(b);
+  return sgn(cross(a, b)) > 0;
+}
--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -1,21 +1,23 @@
+/**
+ * Opis: Podstawowy szablon do geometrii.
+ * Do wszystkich porównań należy używać `sgn`.
+ */
 #pragma once
 
-struct pt {
-  ll x, y;
-  pt operator+(pt o) const { return {x + o.x, y + o.y}; }
-  pt operator-(pt o) const { return {x - o.x, y - o.y}; }
-  pt operator*(ll a) const { return {x * a, y * a}; }
-  pt operator/(ll a) const { return {x / a, y / a}; }
-  auto operator<=>(const pt&) const = default;
-  friend ll cross(pt a, pt b) { return a.x * b.y - a.y * b.x; }
-  friend ll dot(pt a, pt b) { return a.x * b.x + a.y * b.y; }
-  friend ll norm(pt a) { return a.x * a.x + a.y * a.y; }
-  friend int half(pt a) {
-    if (a.y < 0) return -1;
-    if (a.y == 0 && a.x >= 0) return 0;
-    return 1;
-  }
-  friend auto& operator<<(auto& o, pt a) {
-    return o << '(' << a.x << ", " << a.y << ')';
-  }
+using D = ll;
+const D EPS = D(1e-9);
+int sgn(D x) { return (x > EPS) - (x < -EPS); }
+struct P {
+  D x, y;
+  P operator+(P o) const { return {x + o.x, y + o.y}; }
+  P operator-(P o) const { return {x - o.x, y - o.y}; }
+  P operator*(D a) const { return {x * a, y * a}; }
+  P operator/(D a) const { return {x / a, y / a}; }
+  auto operator<=>(const P&) const = default;
 };
+D cross(P a, P b) { return a.x * b.y - a.y * b.x; }
+D dot(P a, P b) { return a.x * b.x + a.y * b.y; }
+D norm(P a) { return a.x * a.x + a.y * a.y; }
+auto& operator<<(auto& o, P a) {
+  return o << '(' << a.x << ", " << a.y << ')';
+}
