3c3702b3013a72c5ed2215760a3f5756b8260ffd
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-05-28 18:10:52 +0200

    Update ConvexHull

--- content/geometry/ConvexHull.h
+++ content/geometry/ConvexHull.h
@@ -5,16 +5,18 @@
  */
 #pragma once
 
-vector<pt> convex_hull(vector<pt> p) {
+#include "Point.h"
+
+vector<P> convex_hull(vector<P> p) {
   if (ssize(p) <= 1) return p;
   sort(p.begin(), p.end());
-  vector<pt> h(ssize(p) + 1);
+  vector<P> h(ssize(p) + 1);
   int s = 0, t = 0;
   for (int it = 0; it < 2; it++) {
-    for (pt a : p) {
+    for (P a : p) {
       while (t >= s + 2) {
-        pt u = h[t - 2], v = h[t - 1];
-        if (cross(v - u, a - v) <= 0) t--;
+        P u = h[t - 2], v = h[t - 1];
+        if (sgn(cross(v - u, a - v)) <= 0) t--;
         else break;
       }
       h[t++] = a;
--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -13,7 +13,12 @@ struct P {
   P operator-(P o) const { return {x - o.x, y - o.y}; }
   P operator*(D a) const { return {x * a, y * a}; }
   P operator/(D a) const { return {x / a, y / a}; }
-  auto operator<=>(const P&) const = default;
+  auto operator<=>(P o) const {
+    return pair(sgn(x - o.x), sgn(y - o.y)) <=> pair(0, 0);
+  }
+  bool operator==(P o) const {
+    return sgn(x - o.x) == 0 && sgn(y - o.y) == 0;
+  }
 };
 D cross(P a, P b) { return a.x * b.y - a.y * b.x; }
 D dot(P a, P b) { return a.x * b.x + a.y * b.y; }
