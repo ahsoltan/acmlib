7226a3382754a01e5a75664b66271d5feafd5f41
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-05-28 18:31:55 +0200

    Update PolygonTangents

--- content/geometry/PolygonTangents.h
+++ content/geometry/PolygonTangents.h
@@ -6,17 +6,19 @@
  */
 #pragma once
 
-pair<pt, pt> tangents(const vector<pt>& p, pt a) {
+#include "Point.h"
+
+pair<P, P> tangents(const vector<P>& p, P a) {
   int n = ssize(p);
-  pt t[2];
+  P t[2];
   for (int it = 0; it < 2; it++) {
     auto dir = [&](int i) {
-      pt u = p[i] - a;
-      pt v = p[i < n - 1 ? i + 1 : 0] - a;
-      ll c = cross(u, v);
+      P u = p[i] - a;
+      P v = p[i < n - 1 ? i + 1 : 0] - a;
+      int c = sgn(cross(u, v));
       if (c != 0) return c < 0;
-      if (dot(u, v) > 0) return norm(u) > norm(v);
-      return true;
+      if (sgn(dot(u, v)) <= 0) return true;
+      return sgn(norm(u) - norm(v)) > 0;
     };
     auto dirx = [&](int i) { return dir(i) ^ it; };
     if (dirx(0) == 1 && dirx(n - 1) == 0) {
@@ -30,12 +32,11 @@ pair<pt, pt> tangents(const vector<pt>& p, pt a) {
       if (dirx(s[x ^ 1]) == (x ^ 1)) {
         s[x] = mid;
       } else {
-        ((cross(p[mid] - a, p[s[1]] - a) < 0) ^ it
-             ? s[x]
-             : s[x ^ 1]) = mid;
+        bool b = sgn(cross(p[mid] - a, p[s[1]] - a)) < 0;
+        s[b ^ x ^ it ^ 1] = mid;
       }
     }
-    t[it] = dirx(s[0] + 1) == 0 ? p[s[0] + 2] : p[s[0] + 1];
+    t[it] = p[s[0] + 1 + (dirx(s[0] + 1) == 0)];
   }
   return {t[0], t[1]};
 }
