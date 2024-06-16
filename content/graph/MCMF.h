8930937b7ebe4a3aec4950915d8b24e58b287616
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-06-16 16:56:23 +0200

    Cleanup MCMF

--- content/graph/MCMF.h
+++ content/graph/MCMF.h
@@ -7,7 +7,7 @@
 #pragma once
 
 #include <ext/pb_ds/priority_queue.hpp>
-const ll INF64 = 2e18;
+const ll INF = 2e18;
 struct MCMF {
   struct edge {
     int from, to, rev;
@@ -19,19 +19,14 @@ struct MCMF {
   __gnu_pbds::priority_queue<pair<ll, int>> q;
   vector<decltype(q)::point_iterator> it;
   vector<edge*> p;
-  MCMF(int _n) {
-    n = _n;
-    adj.resize(n);
-    pi.resize(n);
-    p.resize(n);
-  }
-  void add_edge(int u, int v, ll cap, ll cost, ll rcap = 0) {
+  MCMF(int _n) : n(_n), adj(n), pi(n), p(n) {}
+  void add_edge(int u, int v, ll cap, ll cost) {
     int i = ssize(adj[u]), j = ssize(adj[v]);
     adj[u].push_back({u, v, j + (u == v), cap, cost});
-    adj[v].push_back({v, u, i, rcap, -cost});
+    adj[v].push_back({v, u, i, 0, -cost});
   }
   bool path(int s, int t) {
-    dst.assign(n, INF64); it.assign(n, q.end());
+    dst.assign(n, INF); it.assign(n, q.end());
     q.push({dst[s] = 0, s});
     while (!q.empty()) {
       int u = q.top().second; q.pop();
@@ -48,15 +43,15 @@ struct MCMF {
       }
     }
     for (int i = 0; i < n; i++) {
-      pi[i] = min(pi[i] + dst[i], INF64);
+      pi[i] = min(pi[i] + dst[i], INF);
     }
-    return pi[t] != INF64;
+    return pi[t] != INF;
   }
   pair<ll, ll> flow(int s, int t, ll cap) {
     ll f = 0, c = 0;
     while (f < cap && path(s, t)) {
       ll d = cap - f;
-      for (edge* e = p[t]; e; e = p[e->from]) d = min(d, e->cap);
+      for (edge* e = p[t]; e; e = p[e->from]) d=min(d, e->cap);
       for (edge* e = p[t]; e; e = p[e->from]) {
         e->cap -= d, adj[e->to][e->rev].cap += d;
       }
